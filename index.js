3e7ccfa513ebdcf703caaac9bcb92b397ffcbdf9
MarsMwau <marthamumbua16@gmail.com>
2023-03-13 18:01:19 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+{let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);}
 
-// Write your code here
+{let random = Math.ceil(Math.random() * 100);
+console.log(random);}
+
+{let num3 = 10;
+    let num4 = 6;
+    let mod = num3 % num4;
+    console.log(mod); 
+    }
+{let numbers = [7, 4, 10, 20, 15, 19];
+    let max = Math.max(...numbers);
+    
+    console.log(max);
+    }
+
